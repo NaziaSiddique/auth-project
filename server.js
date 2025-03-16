4b0bb7008e586600ff561f187cd9915a22135b12
NaziaSiddique <nazia.siddique04@gmail.com>
2025-03-16 12:49:03 +0000

    Integrated authentication routes into the main server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
+const authRoutes = require("./controllers/authController");
+app.use("/auth", authRoutes);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("MongoDB Connection Error:", err));
