ee1d52374a7c6a38d7ff74b08f75c31f19bf3c36
NaziaSiddique <nazia.siddique04@gmail.com>
2025-03-16 12:09:33 +0000

    Added authntication service with user registration and login logic

---
+++ services/authService.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const userRepository = require("../repositories/userRepository");
+
+const registerUser = async (username, findUserByEmail, password) => {
+    const existingUser = await userRepository.findUserByEmail(email);
+    if(existingUser) throw new Error("User already exists");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return await userRepository.createUser({ username, email, password: hashedPassword });
+};
+
+const loginUser = async (email, password) => {
+    const user = await userRepository.findUserByEmail(email);
+    if(!user) throw new Error("Invalid email or password");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("Invalid email or password");
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: "1hr"})
+    return token;
+}
+
+module.exports = { registeredUser, loginUser };
